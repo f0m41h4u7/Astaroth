4b860521c99518135074955362b48b2a94c4c7e1
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-07-21 21:38:24 +0300

    disk data

---
+++ internal/collector/disk_info.go
@@ -0,0 +1,67 @@
+package collector
+
+import (
+	"errors"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/f0m41h4u7/Astaroth/pkg/api"
+)
+
+var (
+	ErrWrongDiskData    = errors.New("cannot parse disk data")
+	ErrCannotParseUsed  = errors.New("cannot parse used percentage")
+	ErrCannotParseInode = errors.New("cannot parse inode percentage")
+)
+
+func GetDiskData() (*api.DiskData, error) {
+	mb, err := exec.Command("df", "-k").Output()
+	if err != nil {
+		return nil, err
+	}
+	inode, err := exec.Command("df", "-i").Output()
+	if err != nil {
+		return nil, err
+	}
+	return parseDiskData(string(mb), string(inode))
+}
+
+func parseDiskData(mb string, inode string) (*api.DiskData, error) {
+	mbLines := strings.Split(mb, "\n")[1:]
+	inodeLines := strings.Split(inode, "\n")[1:]
+	disk := api.DiskData{
+		FilesystemData: make([]*api.FilesystemData, len(mbLines)),
+	}
+
+	if (len(mb) == 0) || (len(inode) == 0) {
+		return &disk, ErrWrongDiskData
+	}
+
+	if len(mbLines) != len(inodeLines) {
+		return &disk, ErrWrongDiskData
+	}
+	for i := 0; i < len(mbLines); i++ {
+		fields := strings.Fields(strings.TrimSpace(mbLines[i]))
+		fs := fields[0]
+		used, err := strconv.ParseInt(strings.Split(fields[4], "%")[0], 10, 64)
+		if err != nil {
+			return &disk, ErrCannotParseUsed
+		}
+		fields = strings.Fields(strings.TrimSpace(inodeLines[i]))
+		if fs != fields[0] {
+			return &disk, ErrWrongDiskData
+		}
+		iused, err := strconv.ParseInt(strings.Split(fields[4], "%")[0], 10, 64)
+		if err != nil {
+			return &disk, ErrCannotParseInode
+		}
+
+		disk.FilesystemData[i] = &api.FilesystemData{
+			Filesystem: fs,
+			Used:       used,
+			Inode:      iused,
+		}
+	}
+	return &disk, nil
+}
--- internal/collector/stats_test.go
+++ internal/collector/stats_test.go
@@ -41,3 +41,42 @@ func TestLoadAvg(t *testing.T) {
 		require.NotNil(t, err)
 	})
 }
+
+func TestDiskData(t *testing.T) {
+	t.Run("simple", func(t *testing.T) {
+		mb := `Filesystem            1K-blocks    Used Available Use% Mounted on
+main/containers/proxy  12043904 1202560  10841344  10% /
+none                        492       4       488   1% /dev
+udev                     488476       0    488476   0% /dev/tty
+tmpfs                       100       0       100   0% /dev/lxd
+tmpfs                       100       0       100   0% /dev/.lxd-mounts
+tmpfs                    507972      12    507960   1% /dev/shm
+tmpfs                    507972   51068    456904  11% /run
+tmpfs                      5120       0      5120   0% /run/lock
+tmpfs                    507972       0    507972   0% /sys/fs/cgroup
+tmpfs                    101596       0    101596   0% /run/user/0`
+
+		inode := `Filesystem              Inodes  IUsed    IFree IUse% Mounted on
+main/containers/proxy 21809084 126349 21682735    1% /
+none                    126993     26   126967    1% /dev
+udev                    122119    623   121496    1% /dev/tty
+tmpfs                   126993      1   126992    1% /dev/lxd
+tmpfs                   126993      4   126989    1% /dev/.lxd-mounts
+tmpfs                   126993      4   126989    1% /dev/shm
+tmpfs                   126993    179   126814    1% /run
+tmpfs                   126993      3   126990    1% /run/lock
+tmpfs                   126993     16   126977    1% /sys/fs/cgroup
+tmpfs                   126993      5   126988    1% /run/user/0`
+		data, err := parseDiskData(mb, inode)
+		require.Nil(t, err)
+		require.Equal(t, 10, len(data.FilesystemData))
+		require.Equal(t, "main/containers/proxy", data.FilesystemData[0].Filesystem)
+		require.Equal(t, int64(11), data.FilesystemData[6].Used)
+		require.Equal(t, int64(1), data.FilesystemData[3].Inode)
+	})
+
+	t.Run("empty data", func(t *testing.T) {
+		_, err := parseDiskData("", "")
+		require.NotNil(t, err)
+	})
+}
