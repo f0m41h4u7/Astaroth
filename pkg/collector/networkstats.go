68670e65c4789ef6ba5a3522fd5ff4b5e2c38b3d
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-08-26 23:57:38 +0300

    remove common.go

--- pkg/collector/networkstats.go
+++ pkg/collector/networkstats.go
@@ -48,3 +48,15 @@ func (n *NetworkStatsMetric) Average(wg *sync.WaitGroup, st *api.Stats, snapshot
 	st.NetworkStats.TCPConnStates.TIME_WAIT /= size
 	st.NetworkStats.TCPConnStates.CLOSE_WAIT /= size
 }
+
+func newMap() map[string]int64 {
+	states := map[string]int64{}
+	states["LISTEN"] = 0
+	states["ESTAB"] = 0
+	states["FIN-WAIT"] = 0
+	states["SYN-RCV"] = 0
+	states["TIME-WAIT"] = 0
+	states["CLOSE-WAIT"] = 0
+
+	return states
+}
